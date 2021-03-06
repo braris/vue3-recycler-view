89037a8800d6ed3ca98c4bf704020c77a8434ac8
Denis Pavlishin <pavlishin@nura.biz>
2021-03-06 10:49:42 +0200

    #1: Create default vue-sfc-rollup project

---
+++ dev/serve.vue
@@ -0,0 +1,17 @@
+<script lang="ts">
+import { defineComponent } from 'vue';
+import Vue3RecyclerView from '@/vue3-recycler-view.vue';
+
+export default defineComponent({
+  name: 'ServeDev',
+  components: {
+    Vue3RecyclerView
+  }
+});
+</script>
+
+<template>
+  <div id="app">
+    <vue3-recycler-view />
+  </div>
+</template>
